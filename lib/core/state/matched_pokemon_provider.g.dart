d9f2010466f417e26543528d433e51e99f411aae
go <go5go69@gmail.com>
2024-06-07 22:42:48 +0900

    ✨ マッチしたポケモンを保持するプロバイダーの作成

---
+++ lib/core/state/matched_pokemon_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'matched_pokemon_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$matchedPokemonProviderHash() =>
+    r'c468da35b064bb2e49c2b5b469d48d372ae04655';
+
+/// See also [MatchedPokemonProvider].
+@ProviderFor(MatchedPokemonProvider)
+final matchedPokemonProviderProvider =
+    NotifierProvider<MatchedPokemonProvider, List<Pokemon>>.internal(
+  MatchedPokemonProvider.new,
+  name: r'matchedPokemonProviderProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$matchedPokemonProviderHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MatchedPokemonProvider = Notifier<List<Pokemon>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
